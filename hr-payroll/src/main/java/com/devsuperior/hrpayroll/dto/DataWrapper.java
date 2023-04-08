661b217d6332106b18a4c63688743235e3de1370
RafaelClaumann <rafael_claumann@hotmail.com>
2023-04-08 17:20:25 -0300

    fallback basico com resilience4j

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/dto/DataWrapper.java
@@ -0,0 +1,20 @@
+package com.devsuperior.hrpayroll.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@ToString
+public class DataWrapper<T> {
+
+    private T data;
+
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -3,11 +3,16 @@ package com.devsuperior.hrpayroll.services;
 import com.devsuperior.hrpayroll.dto.response.WorkerDTO;
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.feign.WorkerFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaymentService.class);
 
     private final WorkerFeignClient workerFeignClient;
 
@@ -15,6 +20,7 @@ public class PaymentService {
         this.workerFeignClient = workerFeignClient;
     }
 
+    @CircuitBreaker(name = "hr-worker-get-worker", fallbackMethod = "fallback")
     public Payment generatePayment(final Long workerId, final Integer days) {
         final ResponseEntity<WorkerDTO> responseEntity = this.workerFeignClient.getWorkers(workerId);
         final WorkerDTO worker = responseEntity.getBody();
@@ -22,6 +28,15 @@ public class PaymentService {
         return Payment.builder()
                 .name(worker.getName())
                 .dailyIncome(worker.getDailyIncome())
+                .days(days)
+                .build();
+    }
+
+    private Payment fallback(final Long workerId, final Integer days, Throwable throwable) {
+        LOGGER.error("error detail message: {} ", throwable.getMessage());
+        return Payment.builder()
+                .name("FALLBACK-WORKER-NAME")
+                .dailyIncome(0.00)
                 .days(days)
                 .build();
     }
