54d571fc95252fb40163b88de5b1c4005aa3a8f8
RafaelClaumann <rafael_claumann@hotmail.com>
2023-04-11 07:03:36 -0300

    remove annotation @SpringBootTest
    removendo anotação @SpringBootTest de todos os projetos
    para não subir todo o contexto do spring durante a execução
    dos testes unitarios que nem existem

--- hr-api-gateway/src/test/java/com/devsuperior/hrapigateway/HrApiGatewayApplicationTests.java
+++ hr-api-gateway/src/test/java/com/devsuperior/hrapigateway/HrApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
 package com.devsuperior.hrapigateway;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HrApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void dummyTest() {
+        Assertions.assertTrue(Boolean.TRUE);
+    }
 
 }
--- hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.devsuperior.hreurekaserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HrEurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void dummyTest() {
+        Assertions.assertTrue(Boolean.TRUE);
+    }
 
 }
